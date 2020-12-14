aa13024f5cb673eec4b294c3ad9bf7631ed9c238
hyunjoogo <73145963+hyunjoogo@users.noreply.github.com>
2020-12-14 23:22:29 +0900

    JAVA : clock 완료
    clear 버튼은 아직

---
+++ clock.js
@@ -0,0 +1,18 @@
+const clockDateScreen = document.getElementById('date');
+const clockTimeScreen = document.getElementById('time');
+
+function nowTime() {
+  const now = new Date();
+  const dateOptions = {month: 'numeric', day: 'numeric', weekday: 'long'};
+  const date = new Intl.DateTimeFormat('ko-KR', dateOptions).format(now);
+  clockDateScreen.innerText = date;
+  const timeOptions = {hour: 'numeric', minute: 'numeric', hour12: false};
+  const time = new Intl.DateTimeFormat('ko-KR', timeOptions).format(now);
+  clockTimeScreen.innerText = time;
+}
+function init() {
+  nowTime();
+  setInterval(nowTime, 1000);
+}
+
+init();
