726654d651ccae17f912879d414e85366809dd3f
hyunjoogo <73145963+hyunjoogo@users.noreply.github.com>
2020-12-15 14:10:19 +0900

    JS : todo 저장 이슈
    1. 페이지 refresh 이후에 버튼 Value 안의 true/false 값이 잘 변경됨.
    (예상 : refresh 이후 loadToDos의 문제일듯함)
    
    페이지를 refresh 하기 전에는 버튼 Value 안의 true/false 값이 잘 변경됨.
    
    2. 페이지 refresh 이후에 할일을 추가하면 data가 다 사라짐.

--- todo.js
+++ todo.js
@@ -1,36 +1,63 @@
-'use strict';
+"use strict";
 
 const list = document.getElementById("list");
 const listText = document.querySelector(".content-text");
 const toDoInput = document.getElementById("add-text");
 const toDoForm = document.querySelector(".toDoForm");
-const CHECK = "fas fa-check-circle";
-const UNCHECK = "far fa-circle";
-const LINE_THROUGH = "lineThrough"
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
 
-const TODOS_LS = 'toDos'
+const TODOS_LS = "toDos";
 
 const toDos = [];
 let id = 0;
 
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode
+    .querySelector(".content-text")
+    .classList.toggle(LINE_THROUGH);
+  toDos[element.id].done = toDos[element.id].done ? false : true;
+}
+
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  console.log([element.id]);
+  console.log(toDos[element.id]);
+  toDos[element.id].trash = true;
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+list.addEventListener("click", function (event) {
+  const element = event.target;
+  const elementJob = element.attributes.job.value;
+  console.log(element);
+  console.log(elementJob);
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+  saveToDos();
+});
+
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   toDoInput.value = "";
-  showToDo(currentValue);
-  if(currentValue) {
+  if (currentValue) {
     addToDo(currentValue, id, false, false);
     const toDoObj = {
       name: currentValue,
       id: id,
       done: false,
-      trash: false
-    }
+      trash: false,
+    };
     toDos.push(toDoObj);
     saveToDos();
     id++;
@@ -38,32 +65,34 @@ function handleSubmit(event) {
 }
 
 function addToDo(toDo, id, done, trash) {
-  if(trash) {return;}
+  if (trash) {
+    return;
+  }
   const DONE = done ? CHECK : UNCHECK;
   const LINE = done ? LINE_THROUGH : "";
   const text = `<li class="content-item">
-                  <i class="${DONE} complete" job="complete" id="${id}"></i>
+                  <i class="far ${DONE} complete" job="complete" id="${id}"></i>
                   <p class="content-text ${LINE}">${toDo}</p>
-                  <i class="far fa-trash-alt" job="complete" id="${id}"></i>
-                </li>`
+                  <i class="far fa-trash-alt delete" job="delete" id="${id}"></i>
+                </li>`;
   const position = "beforeend";
   list.insertAdjacentHTML(position, text);
-  
 }
 
 function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parseToDos = JSON.parse(loadedToDos);
-    parseToDos.forEach(function(toDo) {
-      addToDo(toDo.name);
+  const data = localStorage.getItem(TODOS_LS);
+  if (data !== null) {
+    const parseToDos = JSON.parse(data);
+    parseToDos.forEach(function (toDo) {
+      addToDo(toDo.name, toDo.id, toDo.done, toDo.trash);
     });
   }
 }
 
+toDoForm.addEventListener("submit", handleSubmit);
+
 function init() {
   loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
