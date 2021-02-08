ae083e048dfd1fe545688af1ef239d19f827bd74
hyunjoogo <hyunjoogo@gmail.com>
2021-02-08 15:19:03 +0900

    1편 내용

---
+++ paintboard/#1 1편 내용/src/index.js
@@ -0,0 +1,26 @@
+const colors = document.querySelectorAll(".colors");
+const activeBtn = document.querySelectorAll(".activeBtn");
+const brushSize = document.querySelector(".bruschsize");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+let drawing = false;
+
+ctx.lineWidth = 30;
+ctx.lineCap = "round";
+ctx.strokeStyle = "black";
+
+const draw = e => {
+  const x = e.clientX;
+  const y = e.clientY;
+  if (!drawing) return;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+};
+
+canvas.addEventListener("mouseout", () => (drawing = false));
+canvas.addEventListener("mousedown", () => (drawing = true));
+canvas.addEventListener("mouseup", () => (drawing = false));
+canvas.addEventListener("mousemove", draw);
